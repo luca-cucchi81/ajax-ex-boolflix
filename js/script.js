c1e86dac579833f2974630afd95d65e208947694
Luca <lu.cucchi@gmail.com>
2020-03-18 15:33:38 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -9,10 +9,16 @@ $('button').click(function () {
 
 function search() {
     var query = $('#query').val();
+    resetSearch();
+
     var api_key = 'b493320b6020648e92f5a77b43f9e0f7';
     var urlMovie = 'https://api.themoviedb.org/3/search/movie';
 
     getData(query, api_key, urlMovie)
+}
+
+function resetSearch() {
+    $('#query').val('');
 }
 
 function getData(ricerca, api_key, url) {
@@ -27,12 +33,13 @@ function getData(ricerca, api_key, url) {
         success: function (data) {
             var risultati = data.results;
             for (var i = 0; i < risultati.length; i++) {
-                for (var key in risultati[i]) {
+                $(this).each(function () {
                     console.log(risultati[i].title);
                     console.log(risultati[i].original_title);
                     console.log(risultati[i].original_language);
                     console.log(risultati[i].vote_average);
-                }
+                });
+
             }
         },
         error: function (error) {
@@ -41,10 +48,18 @@ function getData(ricerca, api_key, url) {
     });
 }
 
-
-
-
-
+function printStar(num) {
+    var num = Math.ceil(num / 2);
+    var star= '';
+    for (var i = 1; i <= 5; i++) {
+        if(i <= num){
+            star == '<i class="fas fa-star"></i>';
+        }else {
+            star == '<i class="far fa-star"></i>';
+        }
+    }
+    return star
+}
 
 
 
